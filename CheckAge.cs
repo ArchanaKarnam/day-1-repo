e8068b839cb69e86d5f4519f426afcf1793d20d6
Archana Karnam <karnamarchana@gmail.com>
2022-01-31 17:17:30 +0530

    programs

---
+++ CheckAge.cs
@@ -0,0 +1,22 @@
+namespace checkAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.Write("Enter your age : ");
+            age = int.Parse(Console.ReadLine());
+            if (i == 18)
+            {
+                Console.Write("Age is 18");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("Age is not 18");
+                Console.Read();
+            }
+        }
+    }
+}
